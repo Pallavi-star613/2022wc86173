8f4ca0c354335db14fa1a6d8f74f5b8654312906
Pallavi-star613 <2022wc86173@wilp.bits-pilani.ac.in>
2026-09-22 00:36:50 +0530

    Update output message in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("DevOps Continuous Integration Lab");
+        System.out.println("DevOps Continuous Integration Lab - Updated");
     }
 }
