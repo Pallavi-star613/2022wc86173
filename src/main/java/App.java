ed710f6b9807bbf5457047d8c778b06dc7c28a86
Pallavi-star613 <2022wc86173@wilp.bits-pilani.ac.in>
2026-09-22 00:38:30 +0530

    Fix typo in output message case

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("DevOps Continuous Integration Lab - Updated");
+        System.out.println("DevOps Continuous Integration Lab - updated");
     }
 }
